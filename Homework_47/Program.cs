753684aceec664907e7992614aa38cec9e43caf3
Vera Maria <mymadonna89@mail.ru>
2023-03-05 21:31:50 +0600

    Homework 47,  52 files  solved

--- Homework_47/Program.cs
+++ Homework_47/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        static void Print(string word)
+        {
+            System.Console.WriteLine(word);
+        }
+            Print("Задайте двумерный массив размером m x n, заполненный случайными вещественными числами.");
+
+        void FillMatrix(Double[,] matr)
+        {   
+            Random rand = new Random();
+
+            for (int rows = 0; rows < matr.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matr.GetLength(1); columns++)
+                {
+
+                    matr[rows, columns] = Math.Round(rand.NextDouble() * 100, 2);
+
+                }
+            }
+        }
+
+            void PrintMatrix(Double[,] matr)
+            {
+                for (int rows = 0; rows < matr.GetLength(0); rows++)
+                {
+                    for (int columns = 0; columns < matr.GetLength(1); columns++)
+                    {
+                        Console.Write($"{matr[rows, columns]}\t");
+
+                    }
+                    System.Console.WriteLine();
+                }
+            }
+             Double[,] matrix = new Double[3, 4];
+             
+              FillMatrix(matrix);
+              PrintMatrix(matrix);
+
+        
+
--- Homework_52/Program.cs
+++ Homework_52/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Print(string word)
+        {
+            System.Console.WriteLine(word);
+        }
+            Print("Найдите среднее арифметическое элементов в каждом столбце.");
+
+        void FillMatrix(int [,] matr)
+        { Print("Заполните массив целыми числами");
+            Random rand = new Random();
+            for (int rows = 0; rows < matr.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matr.GetLength(1); columns++)
+                {
+
+                    matr[rows, columns] = rand.Next(1, 10);
+
+                }
+            }
+        }
+
+            void PrintMatrix(int [,] matr)
+            { Print("Вывод массива");
+                for (int rows = 0; rows < matr.GetLength(0); rows++)
+                {
+                    for (int columns = 0; columns < matr.GetLength(1); columns++)
+                    {
+                        Console.Write($"{matr[rows, columns]}\t");
+
+                    }
+                    System.Console.WriteLine();
+                }   
+            }
+
+          void SumMatrix(int [,] matr)
+            {  Print("Среднее арифметическое каждого столбца");
+                 for (int rows = 0; rows < matr.GetLength(1); rows++)
+                {     Double summa = 0;
+                    for (int columns = 0; columns < matr.GetLength(0); columns++)
+                    {
+                       summa = summa + matr[columns, rows];
+                    }
+                     Double sumOfCol = summa/matr.GetLength(0);
+                        sumOfCol = Math.Round(sumOfCol, 1);
+                    System.Console.WriteLine(sumOfCol);
+                }   
+            }
+
+
+             int [,] matrix = new int [3, 4];
+             
+              FillMatrix(matrix);
+              PrintMatrix(matrix);
+              SumMatrix(matrix);
+                
+        
+
